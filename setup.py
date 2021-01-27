bc071f666cca2a9ed362a06a022fea2d084bf480
chrimaho <chrismahoney@hotmail.com>
2021-01-27 18:57:25 +1100

    Add cookiecutter files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MDSI - Advanced Data Science for Innovation - February 2021 - Assessment Task 01',
+    author='Chris Mahoney',
+    license='MIT',
+)
