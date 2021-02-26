a3fa3a0421b248cab6fad4611f7218aa770cb708
chrimaho <chrismahoney@hotmail.com>
2021-02-27 09:26:02 +1100

    Add more assertions

--- src/utils/assertions.py
+++ src/utils/assertions.py
@@ -1,3 +1,29 @@
+#------------------------------------------------------------------------------#
+# is_*() functions                                                           ####
+#------------------------------------------------------------------------------#
+
+
+def is_dataframe(obj):
+    import pandas as pd
+    return isinstance(obj, pd.DataFrame)
+
+
+def is_series(obj):
+    import pandas as pd
+    return isinstance(obj, pd.Series)
+
+
+def is_ndarray(obj):
+    import numpy as np
+    return isinstance(obj, np.ndarray)
+
+
+
+#------------------------------------------------------------------------------#
+# all_*() functions                                                         ####
+#------------------------------------------------------------------------------#
+
+
 def all_str(lst):
     if isinstance(lst, list):
         return all([isinstance(element, str) for element in lst])
@@ -19,6 +45,13 @@ def all_int(lst):
         return isinstance(lst, int)
 
 
+def all_positive(lst):
+    if isinstance(lst, list):
+        return all([element > 0 for element in lst])
+    else:
+        return lst > 0
+
+
 def all_dict(lst):
     if isinstance(lst, list):
         return all([isinstance(element, dict) for element in lst])
@@ -35,19 +68,17 @@ def all_real(lst):
 
 
 def all_dataframe(lst):
-    import pandas as pd
     if isinstance(lst, list):
-        return all([isinstance(element, pd.DataFrame) for element in lst])
+        return all([is_dataframe(element) for element in lst])
     else:
-        return isinstance(lst, pd.DataFrame)
+        return is_dataframe(lst)
 
 
 def all_ndarray(lst):
-    import numpy as np
-    if isinstance(lst, np.ndarray):
-        return all([isinstance(element, np.ndarray) for element in lst])
+    if isinstance(lst, list):
+        return all([is_ndarray(element) for element in lst])
     else:
-        return isinstance(lst, np.ndarray)
+        return is_ndarray(lst)
 
 
 def all_dataframe_or_series(lst):
@@ -56,6 +87,7 @@ def all_dataframe_or_series(lst):
         return all([isinstance(element, (pd.DataFrame, pd.Series)) for element in lst])
     else:
         return isinstance(lst, (pd.DataFrame, pd.Series))
+
     
 def all_dataframe_or_series_or_ndarray(lst):
     import pandas as pd
@@ -64,8 +96,10 @@ def all_dataframe_or_series_or_ndarray(lst):
         return all([isinstance(element, (pd.DataFrame, pd.Series, np.ndarray)) for element in lst])
     else:
         return isinstance(lst, (pd.DataFrame, pd.Series, np.ndarray))
+
     
 def is_in(elem, lst):
     assert not hasattr(elem, "__len__")
     assert hasattr(lst, "__len__")
     return elem in lst
+
